995561f7adba54bbee27d27c4dad6f987c2feed5
Kaenna <laykae@outlook.com>
2026-05-17 18:43:42 -0300

    Adicionando o banco de dados

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 #Iniciando o flask e conectando o banco de dados
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app= Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///healthypet.db'
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
+
+from routes import *
+from models import *
+
+class User(db.Model):
+    with app.app.context():
+        db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
